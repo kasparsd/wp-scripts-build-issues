4987f2362ae757d9f4818f70f6b23018557f06c0
Kaspars Dambis <hi@kaspars.net>
2025-04-04 16:17:50 +0300

    Track with built

--- build-with-entrypoints/custom.asset.php
+++ build-with-entrypoints/custom.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '31d6cfe0d16ae931b73c');
+<?php return array('dependencies' => array(), 'version' => '480f269280e68863d073');
--- build-with-entrypoints/index.asset.php
+++ build-with-entrypoints/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '31d6cfe0d16ae931b73c');
+<?php return array('dependencies' => array(), 'version' => 'd273bf6006032b9876f8');
